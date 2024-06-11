f8806bc97e45014d2c7ed8383ab37fb579e5df8d
shakil <mdshakilhossain720@gmail.com>
2024-06-11 12:21:36 +0600

    opp

---
+++ oop/classs.py
@@ -0,0 +1,14 @@
+class Employee:
+    def __init__(self, name, age):
+        self.name =  name
+        self.age = age
+
+
+kirk = ["James Kirk", 34, "Captain", 2265]
+spock = ["Spock", 35, "Science Officer", 2254]
+mccoy = ["Leonard McCoy", "Chief Medical Officer", 2266]
+
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
