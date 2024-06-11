ab07452db118df4d3d25ec1f93107e31d3d57232
shakil <mdshakilhossain720@gmail.com>
2024-06-11 11:35:39 +0600

    forloop

--- functions.py
+++ functions.py
@@ -1,5 +1,22 @@
-print('hello')
-def dis():
-    name="Hello Bangladesh"
-    print('it is the ',name)
-dis()
+months = ["jan", "feb", "mar"]
+days = ["sun", "mon", "tue"]
+
+
+for x in months:
+  for y in days:
+    print(x, y)
+
+print("Good bye!")
+
+
+
+i = 2
+while(i < 100):
+   j = 2
+   while(j <= (i/j)):
+      if not(i%j): break
+      j = j + 1
+   if (j > i/j) : print(i, " is prime")
+   i = i + 1
+
+print("Good bye!")
