9a7d3f47f7c83a4b620528b55629129927a35f9c
shakil <mdshakilhossain720@gmail.com>
2024-06-22 10:51:10 +0600

    class

---
+++ oop/param.py
@@ -0,0 +1,17 @@
+
+class Myclass (object):
+    def show(self):
+        print('Hello the Method')
+
+x=Myclass()
+x.show()
+
+
+class Mobile:
+    def __init__(self):
+        self.mobile='Realme'
+
+    def showdata(self):
+        print('MObile',self.mobile)
+realme=Mobile()
+realme.showdata()
